e34458c092f784d7890d4ca5c69b1fca544c9a4c
Manthan Tolia <mtolia9@gmail.com>
2021-01-12 13:52:27 +0530

    feature/summary

--- src/components/MonitorTable.jsx
+++ src/components/MonitorTable.jsx
@@ -3,50 +3,22 @@ import { useState } from 'react';
 import Table from 'react-bootstrap/Table';
 
 export default function MonitorTable(props) {
-    const [onTotal, setOnTotal] = useState(null);
-    const [offTotal, setOffTotal] = useState(null);
-
-    const getTotal = () => {
-        let totalOn = new Array(props.header.length - 1).fill(0);
-        let totalOff = new Array(props.header.length - 1).fill(0);
-        props.table.forEach(row => {
-            let nrow = Object.values(row)
-            nrow.forEach((ele, index) => {
-                if (index < nrow.length - 1) {
-                    if (ele === "online")
-                        totalOn[index]++;
-                    if (ele === "offline")
-                        totalOff[index]++;
-                }
-            })
-        })
-        setOnTotal([...totalOn]);
-        setOffTotal([...totalOff]);
-    }
-
-    useEffect(() => {
-        if (props.timer <= props.limit+1 && props.timer!=0)
-            getTotal();
-    }, [props.timer])
-
     return (
-        <div>
-
-            <div style={{ height: '40vh', overflowY: 'scroll', overflowX: 'scroll' }}>
-                {
-                    props.header && <Table striped bordered hover varaint="dark" responsive size="sm">
-                        <thead>
-                            <tr>
-                                <th>S.No</th>
-                                {props.header.map((val, index) => <th key={index}>{val}</th>)}
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                props.table.length > 0 && props.table.map((row, index) => {
-                                    let nrow = Object.values(row)
-                                    return <tr>
-                                        <td>{index+1}</td>
+        <div style={{ height: '40vh', overflowY: 'scroll', overflowX: 'scroll' }}>
+            {
+                props.header && <Table striped bordered hover varaint="dark" responsive size="sm">
+                    <thead>
+                        <tr>
+                            <th>S.No</th>
+                            {props.header.map((val, index) => <th key={index}>{val}</th>)}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            props.table.length > 0 && props.table.map((row, index) => {
+                                let nrow = Object.values(row)
+                                return <tr key={index}>
+                                    <td>{index + 1}</td>
                                     {
                                         nrow.map((ele, index) => {
                                             if (index == 0)
@@ -55,25 +27,12 @@ export default function MonitorTable(props) {
                                                 return <td key={index}>{nrow[index - 1]}</td>
                                         })
                                     }
-                                    </tr>
-                                })
-                            }
-                        </tbody>
-                    </Table>
-                }
-            </div>
-            <Table striped bordered hover varaint="dark" responsive size="sm">
-                <tbody>
-                    <tr>
-                        <td>Online</td>
-                        {props.timer==props.limit && onTotal && onTotal.map((ele, index) => <td key={index} >{ele}</td>)}
-                    </tr>
-                    <tr>
-                        <td>Offline</td>
-                        {props.timer==props.limit && offTotal && offTotal.map((ele, index) => <td key={index} >{ele}</td>)}
-                    </tr>
-                </tbody>
-            </Table>
+                                </tr>
+                            })
+                        }
+                    </tbody>
+                </Table>
+            }
         </div>
     )
 }
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -12,7 +12,7 @@ import MonitorTable from '../MonitorTable';
 import { CSVLink } from "react-csv";
 import moment from 'moment';
 
-export default function Home(props) {
+export default function Home() {
   const [numOfDevices, setNumOfDevices] = useState(0);
   const [cacheNumOfDevices, setCacheNumOfDevices] = useState(0);
   const [deviceIDs, setDeviceIDs] = useState([]);
@@ -20,22 +20,56 @@ export default function Home(props) {
   const [table, setTable] = useState([]);
   const [timer, setTimer] = useState(0);
   const [start, setStart] = useState(false);
-  const [limit] = useState(4);
+  const [limit] = useState(5); //set limit = 150 for 5 minutes
+  const [onTotal, setOnTotal] = useState(null);
+  const [offTotal, setOffTotal] = useState(null);
 
   const getData = async () => {
-    const devices = { 'devices': deviceIDs };
-    const body = JSON.stringify(devices);
-    if (timer == limit)
-     setTimer(1);
-    else
+    if (timer == limit) {
+      var newDataOn = {...table[0]};
+      newDataOn["Timestamp"] = "Online";
+      Object.keys(newDataOn).forEach((key, index) => {
+        if (index<onTotal.length) {
+          newDataOn[key] = onTotal[index];
+        }
+      })
+      var newTable = table;
+      newTable.push(newDataOn);
+      var newDataOff = {...table[1]};
+      newDataOff["Timestamp"] = "Offline"
+      Object.keys(newDataOff).forEach((key, index) => {
+        if (index<offTotal.length) {
+          newDataOff[key] = offTotal[index];
+        }
+      })
+      newTable.push(newDataOff);
+      setTimer(0);
+      setTable([...newTable]);
+    }
+    else {
+      var totalOn, totalOff;
+      totalOn = onTotal ? onTotal : new Array(header.length - 1).fill(0);
+      totalOff = offTotal ? offTotal : new Array(header.length - 1).fill(0);
+      const devices = { 'devices': deviceIDs };
+      const body = JSON.stringify(devices);
       setTimer(timer + 1);
-    const newData = await getStatus(body);
-    const data = { Timestamp: moment(Date.now()).format() };
-    const finalData = Object.assign(data, newData);
-    console.log(finalData);
-    let newTable = table;
-    newTable.push(finalData);
-    setTable([...newTable]);
+      const newData = await getStatus(body);
+      Object.values(newData).forEach((val, index) => {
+        if (val === "online") {
+          totalOn[index]++;
+        }
+        else {
+          totalOff[index]++;
+        }
+      })
+      var data = { Timestamp: moment(Date.now()).format() };
+      var finalData = Object.assign(data, newData);
+      var newTable = table;
+      newTable.push(finalData);
+      setOnTotal([...totalOn]);
+      setOffTotal([...totalOff]);
+      setTable([...newTable]);
+    }
   }
 
   const csvReport = {
@@ -46,9 +80,9 @@ export default function Home(props) {
 
   useEffect(() => {
     if (start) {
-        var interval = setInterval(async () => {
-          await getData();
-        }, 2000);
+      var interval = setInterval(async () => {
+        await getData();
+      }, 2000);
       return () => clearInterval(interval);
     }
   }, [table])
@@ -96,13 +130,13 @@ export default function Home(props) {
         )
       }
       {
-        numOfDevices > 0 && <Button className="Primary" onClick={() =>{ setStart(true); getData()}}>Start</Button>
+        numOfDevices > 0 && <Button className="Primary" onClick={() => { setStart(true); getData() }}>Start</Button>
       }
       {
         start && <Button style={{ marginLeft: '3px' }} className="Primary" onClick={() => setStart(false)}>Stop</Button>
       }
       {
-        <MonitorTable limit={limit} header={header} table={table} timer={timer} /> //set limit = 150 for 5 minutes
+        <MonitorTable limit={limit} header={header} table={table} timer={timer} />
       }
       <CSVLink {...csvReport}>Export to CSV</CSVLink>
     </div>
